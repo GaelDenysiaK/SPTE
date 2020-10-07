f0311b1d9eb2be009f7824010dd9b4352a49f818
Webaxones <contact@webaxones.com>
2020-10-07 22:39:41 +0200

    Correction signe slash doublé Fixes #7

--- spte.js
+++ spte.js
@@ -150,8 +150,8 @@ const rgxBadWords = new RegExp(`(?<=[\\s,.:;"']|^)${data.badWord.map(escapeRegEx
 // Match single quotes.
 const rgxSingleQuotes = new RegExp('(?<!href\\=|href\\=\'[a-z0-9.]*?|%[a-z])\u0027(?!%)', 'gm');
 
-// Match slash with spaces(before and after, breaking or not).
-const rgxSlash = new RegExp(`((?<= | )\\${data.slash}|\\${data.slash}(?= | ))`, 'gmi');
+// Match slash with spaces (before and after, breaking or not) and not preceded or not followed by another slash.
+const rgxSlash = new RegExp(`(?<= | )\\${data.slash}(?!\\${data.slash})|(?<!\\${data.slash})\\${data.slash}(?= | )`, 'gmi');
 
 // Match typographic rules "space before": not preceded by space (breaking or non breaking) and not as first character OR not as first character and followed by space (breaking or non breaking)
 // plus double opening hook case.
