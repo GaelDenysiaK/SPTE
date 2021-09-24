3a804833747f18280f130c59a0ced2e397af66bd
Loïc Antignac <contact@webaxones.com>
2021-09-24 11:34:44 +0200

    correction popup

--- spte.js
+++ spte.js
@@ -530,6 +530,7 @@ function getGlossaryRegex(glossary) {
 }
 
 function mainProcesses(spteSettings) {
+	document.body.appendChild(spPopup);
 	gpContentMaxWidth(spteSettings.spteGpcontentBig, spteSettings.spteGpcontentMaxWitdh);
 	if (spteSettings.spteBetterReadability && spteSettings.spteBetterReadability === 'true') { document.body.classList.add('sp-better-readability'); }
 
