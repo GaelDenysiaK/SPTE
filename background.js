e7c5c7aff41d6f4f8bb3005111424e75d8b767ae
Loïc Antignac <contact@webaxones.com>
2022-04-03 09:00:10 +0200

    first move to manifest v3

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (!(/https:\/\/translate\.wordpress\.org\//).test(tab.url)) { return; }
 	if (changeInfo.status) {
-		chrome.pageAction.show(tabId);
-		chrome.pageAction.setPopup({
+		chrome.action.show(tabId);
+		chrome.action.setPopup({
 			tabId,
 			popup: '/pageAction/index.html',
 		});
