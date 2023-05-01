0af1dc9895a8ece98ae933b7fb72bd2ca1a7b3ea
Loïc ANTIGNAC <contact@webaxones.com>
2023-05-01 17:42:23 +0200

    change chrome actions to disable enable popup and icon depending on current url

--- background.js
+++ background.js
@@ -1,7 +1,10 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	chrome.action.disable();
+
 	if (!(/https:\/\/translate\.wordpress\.org\//).test(tab.url)) { return; }
+	chrome.action.enable();
+
 	if (changeInfo.status) {
-		chrome.action.show(tabId);
 		chrome.action.setPopup({
 			tabId,
 			popup: '/pageAction/index.html',
